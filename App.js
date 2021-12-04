7f23d3b4480e4aa91bb8e1e32065a17eb42c25ac
vertika12345 <90095282+vertika12345@users.noreply.github.com>
2021-12-04 16:16:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import ResponsiveAppBar from './task1/Navbar';
+//import  TodoList  from './task1/TodoList';
+
+function App() {
+  
+  return ( 
+    <>
+    <ResponsiveAppBar/>
+    </>
+  )
+}
+
+export default App;
